e85b18dfe250a278dc67f8d070e45aab2f18c4a5
mr_tawan@hotmail.com <mr_tawan@hotmail.com>
2024-08-04 21:32:57 +0700

    add slurm sbatch

---
+++ queue_train_job.py
@@ -0,0 +1,48 @@
+import click
+from pathlib import Path
+import logging
+import cv2
+from simple_slurm import Slurm
+
+
+@click.command()
+@click.argument('project')
+def cli(project):
+    """Queue training job."""
+
+    project_path = Path.cwd() / "workspace" / project
+    dataset_path = project_path / "dataset"
+
+    if not project_path.exists() or project_path.is_file():
+        logging.error(f"Unable to find project directory: {project_path}")
+        return
+
+    dataset_config_path = project_path / "dataset_config.toml"
+    training_config_path = project_path / "training_config.toml"
+
+    if not dataset_config_path.exists():
+        logging.error(f"Unable to dataset_config.toml: {project_path}")
+        return
+
+    if not training_config_path.exists():
+        logging.error(f"Unable to training_config.toml: {project_path}")
+        return
+
+    slurm = Slurm(
+        gres=['gpu:1'],
+        job_name=f'train sdxl - {project}',
+    )
+
+    slurm.add_cmd('source', f'{Path.cwd()}/venv/bin/activate')
+
+    slurm.sbatch('accelerate', 'launch', '--quiet',
+                 f'--config_file={Path.cwd()}/accelerate_config.yaml',
+                 f'--num_cpu_threads_per_process=1',
+                 f'{Path.cwd()}/sd-scripts/sdxl_train_network.py',
+                 f'--dataset_config={project_path}/dataset_config.toml',
+                 f'--config_file{project_path}/training_config.toml'
+                 )
+
+
+if __name__ == "__main__":
+    cli()
